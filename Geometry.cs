f2dcf3a1821257f4b78815c4f99148bd0d20cbba
Scott Kane <scott-_-kane@live.co.uk>
2019-09-09 22:31:51 +0100

    Initial commit

---
+++ Geometry.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+
+public static class Geometry
+{
+    //Is a triangle in 2d space oriented clockwise or counter-clockwise
+    //https://math.stackexchange.com/questions/1324179/how-to-tell-if-3-connected-points-are-connected-clockwise-or-counter-clockwise
+    //https://en.wikipedia.org/wiki/Curve_orientation
+    public static bool IsTriangleOrientedClockwise(Vector2 p1, Vector2 p2, Vector2 p3)
+    {
+        bool isClockWise = true;
+
+        float determinant = p1.x * p2.y + p3.x * p1.y + p2.x * p3.y - p1.x * p3.y - p3.x * p2.y - p2.x * p1.y;
+
+        if (determinant > 0f)
+        {
+            isClockWise = false;
+        }
+
+        return isClockWise;
+    }
+
+    //Calculate the center of circle in 2d space given three coordinates
+    //http://paulbourke.net/geometry/circlesphere/
+    public static Vector2 CalculateCircleCenter(Vector2 p1, Vector2 p2, Vector2 p3)
+    {
+        Vector2 center = new Vector2();
+
+        float ma = (p2.y - p1.y) / (p2.x - p1.x);
+        float mb = (p3.y - p2.y) / (p3.x - p2.x);
+
+        center.x = (ma * mb * (p1.y - p3.y) + mb * (p1.x + p2.x) - ma * (p2.x + p3.x)) / (2 * (mb - ma));
+        center.y = (-1 / ma) * (center.x - (p1.x + p2.x) / 2) + (p1.y + p2.y) / 2;
+
+        return center;
+    }
+
+    public static Vector2 XZ(this Vector3 v)
+    {
+        return new Vector2(v.x, v.z);
+    }
+
+    //Is a point d inside, outside or on the same circle as a, b, c
+    //https://gamedev.stackexchange.com/questions/71328/how-can-i-add-and-subtract-convex-polygons
+    //Returns positive if inside, negative if outside, and 0 if on the circle
+    public static float IsPointInsideOutsideOrOnCircle(Vector2 aVec, Vector2 bVec, Vector2 cVec, Vector2 dVec)
+    {
+        //This first part will simplify how we calculate the determinant
+        float a = aVec.x - dVec.x;
+        float d = bVec.x - dVec.x;
+        float g = cVec.x - dVec.x;
+
+        float b = aVec.y - dVec.y;
+        float e = bVec.y - dVec.y;
+        float h = cVec.y - dVec.y;
+
+        float c = a * a + b * b;
+        float f = d * d + e * e;
+        float i = g * g + h * h;
+
+        //float c = Mathf.Abs(a) + Mathf.Abs(b);
+        //float f = Mathf.Abs(d) + Mathf.Abs(e);
+        //float i = Mathf.Abs(g) + Mathf.Abs(g);
+
+        float determinant = (a * e * i) + (b * f * g) + (c * d * h) - (g * e * c) - (h * f * a) - (i * d * b);
+        //float determinant = ((a * e) + i) + ((b * g) + f) + ((d * h) + c) - ((g * e) + c) - ((h * a) + f) - ((d * b) + i);
+
+        return determinant;
+    }
+
+    //Is a quadrilateral convex? Assume no 3 points are colinear and the shape doesnt look like an hourglass
+    public static bool IsQuadrilateralConvex(Vector2 a, Vector2 b, Vector2 c, Vector2 d)
+    {
+        bool isConvex = false;
+
+        bool abc = IsTriangleOrientedClockwise(a, b, c);
+        bool abd = IsTriangleOrientedClockwise(a, b, d);
+        bool bcd = IsTriangleOrientedClockwise(b, c, d);
+        bool cad = IsTriangleOrientedClockwise(c, a, d);
+
+        if (abc && abd && bcd & !cad)
+        {
+            isConvex = true;
+        }
+        else if (abc && abd && !bcd & cad)
+        {
+            isConvex = true;
+        }
+        else if (abc && !abd && bcd & cad)
+        {
+            isConvex = true;
+        }
+        //The opposite sign, which makes everything inverted
+        else if (!abc && !abd && !bcd & cad)
+        {
+            isConvex = true;
+        }
+        else if (!abc && !abd && bcd & !cad)
+        {
+            isConvex = true;
+        }
+        else if (!abc && abd && !bcd & !cad)
+        {
+            isConvex = true;
+        }
+
+
+        return isConvex;
+    }
+}
---
+++ VoronoiController.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class VoronoiController : MonoBehaviour
+{
+    public int seed = 0;
+
+    public float halfMapSize = 10f;
+
+    public float numberOfPoints = 20;
+
+    private void OnDrawGizmos()
+    {
+        //Generate the random sites
+        List<Vector3> randomSites = new List<Vector3>();
+
+        //Generate random numbers with a seed
+        Random.InitState(seed);
+
+        float max = halfMapSize;
+        float min = -halfMapSize;
+
+        for (int i = 0; i < numberOfPoints; i++)
+        {
+            float randomX = Random.Range(min, max);
+            float randomZ = Random.Range(min, max);
+
+            randomSites.Add(new Vector3(randomX, 0f, randomZ));
+        }
+
+
+        //Points outside of the screen for voronoi which has some cells that are infinite
+        float bigSize = halfMapSize * 5f;
+
+        //Star shape which will give a better result when a cell is infinite large
+        //When using other shapes, some of the infinite cells misses triangles
+        randomSites.Add(new Vector3(0f, 0f, bigSize));
+        randomSites.Add(new Vector3(0f, 0f, -bigSize));
+        randomSites.Add(new Vector3(bigSize, 0f, 0f));
+        randomSites.Add(new Vector3(-bigSize, 0f, 0f));
+
+
+        //Generate the voronoi diagram
+        List<VoronoiCell> cells = DelaunayToVoronoi.GenerateVoronoiDiagram(randomSites);
+
+
+        //Debug
+        //Display the voronoi diagram
+        DisplayVoronoiCells(cells);
+
+        //Display the sites
+        Gizmos.color = Color.white;
+
+        for (int i = 0; i < randomSites.Count; i++)
+        {
+            float radius = 0.2f;
+
+            Gizmos.DrawSphere(randomSites[i], radius);
+        }
+    }
+
+    //Display the voronoi diagram with mesh
+    private void DisplayVoronoiCells(List<VoronoiCell> cells)
+    {
+        Random.InitState(seed);
+
+        for (int i = 0; i < cells.Count; i++)
+        {
+            VoronoiCell c = cells[i];
+
+            Vector3 p1 = c.sitePos;
+
+            Gizmos.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+
+            List<Vector3> vertices = new List<Vector3>();
+
+            List<int> triangles = new List<int>();
+
+            vertices.Add(p1);
+
+            for (int j = 0; j < c.edges.Count; j++)
+            {
+                Vector3 p3 = c.edges[j].v1;
+                Vector3 p2 = c.edges[j].v2;
+
+                vertices.Add(p2);
+                vertices.Add(p3);
+
+                triangles.Add(0);
+                triangles.Add(vertices.Count - 2);
+                triangles.Add(vertices.Count - 1);
+            }
+
+            Mesh triangleMesh = new Mesh();
+
+            triangleMesh.vertices = vertices.ToArray();
+
+            triangleMesh.triangles = triangles.ToArray();
+
+            triangleMesh.RecalculateNormals();
+
+            Gizmos.DrawMesh(triangleMesh);
+        }
+    }
+}
